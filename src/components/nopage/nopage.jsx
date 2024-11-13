4f6d719da1021dc97124fad77b418605f7f75535
Med-EL <simoel2013@gmail.com>
2024-11-13 14:16:47 +0100

    second commit login page done

---
+++ src/components/nopage/nopage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class nopage extends Component {
+  render() {
+    return (
+      <div>
+        
+        <h1>nopage</h1></div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import Auth from './components/auth/auth';
+import Home from './components/home/home';
+import NoPage from './components/nopage/nopage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<Router>
+      <Routes>
+        {/* Default Route to Auth */}
+        <Route path="/" element={<Navigate to="/auth" />} />
+        
+        {/* Other Routes */}
+        <Route path="/auth" element={<Auth />} />
+
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+</Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
